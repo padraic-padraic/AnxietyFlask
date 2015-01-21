7b5ca5e2a6805a5c76aa2ea3420bcf6ce1e2483f
unknown <phc11@phws319-025.ic.ac.uk>
2015-01-21 12:27:12 +0000

    Seperqate config into a python object

---
+++ AnxietyFlask/config.py
@@ -0,0 +1,10 @@
+class Config():
+	DEBUG=False
+	SQLALCHEMY_DATABASE_URL = 'sqlite:///tmp/test.db'
+	MAIL_SERVER = 'localhost'
+	MAIL_USE_SSL = True
+	DEFAULT_MAIL_SENDER = 'your_anxiety@anxietyfla.sk'
+
+class Testing(Config):
+	DEBUG = True
+	MAIL_DEBUG = True
