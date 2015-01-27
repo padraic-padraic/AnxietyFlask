6579e1d4bebee0d5430b1b067ce026824c9e4712
Padraic Calpin <padraic.calpin93@gmail.com>
2015-01-27 14:53:11 +0000

    Finished Mailgun Module

--- AnxietyFlask/config.py
+++ AnxietyFlask/config.py
@@ -9,6 +9,7 @@ class Testing(Config):
 	DEBUG = True
 	MAIL_DEBUG = True
 
-class Mailgun_Config(Config):
-    API_KEY = ''
-    BASE_URL = ''
+class MailgunConfig(Config):
+    API_KEY = 'key-b0199c3faeb9e1a83f8a6eea519966c2'
+    BASE_URL = 'https://api.mailgun.net/v2/'
+    DOMAIN = 'https://mailgun.com/cp/domains/54c0f70e045ca436f8e9ccb3'
--- AnxietyFlask/mailgun.py
+++ AnxietyFlask/mailgun.py
@@ -1,18 +1,32 @@
 from config import MailgunConfig
+from datetime import datetime, timedelta
+
+from requests import get, post
 
 conf = MailgunConfig()
-
-import requests
 
 class Mail():
     api_key =conf.API_KEY
     base_url = conf.BASE_URL
     domain = conf.DOMAIN
+
     def __init__(self, **kwargs):
         self.parameters = dict(kwargs.items())
 
-    def do_request(self, endpoint):
-        _r = requests.get(base_url+endpoint, params=self.parameters, auth=('api',self.api_key))
+    def add_params(self, **kwargs):
+        for key, val in kwargs:
+            self.parameters[key] = val
+
+    def do_request(self, endpoint, r_type, **kwargs):
+        [self.parameters[key] = val for key, val in kwargs]
+        if r_type == 'get:'
+            _r = get(base_url+endpoint, params=self.parameters,
+                              auth=('api',self.api_key))
+        elif r_type == 'post':
+            _r = post(base_url+endpoint, params=self.parameters,
+                               auth=('api',self.api_key)
+        else:
+            raise Exception
         if _r.status_code != 200:
             raise Exception
         return _r.json()
@@ -20,8 +34,26 @@ class Mail():
 class InMail(Mail):
     base_url = conf.BASE_URL + '/domains/'
     
-    @classmethod:
+    @classmethod
+    def from_dict(_dict):
+        self.parameters = dict(_dict.items())
+
+    @classmethod
     def get_messages(cls):
+        events = Mail(begin=datetime.now()-timedelta(days=1), end=datetime.now(),
+                       pretty='no', event='stored').do_request('/events', 'get')
+        messages = []
+        for item in events['items']:
+            data = InMail().do_request('messages', 'get', key=item['sotrage']['key'])        
+            messages.append(data)
+            messages.append(InMail().from_dict(data))
+        return messages
 
 class OutMail(Mail):
-    pass
+    required = ['to', 'body', 'html', 'subject']
+
+    def send(self):
+        if not any(i in self.parameters.keys() for i in required):
+            raise Exception
+        self.do_request('messages', 'post')
+
